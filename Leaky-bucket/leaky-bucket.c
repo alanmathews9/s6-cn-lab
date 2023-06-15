2270712bfd2487afcb0caf8d07fe898aed82d204
Alan <alangeorgemathews09@gmail.com>
2023-06-15 10:04:56 +0530

    Added kiran maam code

--- Leaky-bucket/leaky-bucket.c
+++ Leaky-bucket/leaky-bucket.c
@@ -1,29 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void main()
 {
-    int in, out, bsize, n bucket = 0;
+    int in, out, bsize, n, bucket = 0;
     printf("Enter the bucket size: ");
     scanf("%d", &bsize);
-    printf("Enter no. of inputs: ")
+    printf("Enter no. of inputs: ");
     scanf("%d", &n);
     printf("Enter the output rate: ");
     scanf("%d", &out);
 
-    while (n!=0)
+    while (n != 0)
     {
-        printf("enter incoming packet size: ");
+        printf("Enter incoming packet size: ");
         scanf("%d", &in);
-        if (in <= (bsize-bucket))
+        if (in <= (bsize - bucket))
         {
-            bucket += in
+            bucket += in; 
             printf("Bucket status: %d out of %d\n", bucket, bsize);
         }
         else
         {
             printf("Dropped packets: %d\n", in - (bsize - bucket));
-            printf("Bucket status: %d out of %d\n", bucket, bsize)
-            bucket = bsize;          //shouldnt this be before the printf?
+            printf("Bucket status: %d out of %d\n", bucket, bsize);
+            bucket = bsize;
         }
         bucket -= out;
         printf("Bucket status after outgoing: %d out of %d\n", bucket, bsize);
